e9f6ca31b0c88d87829e24e8060135612491892f
Fares Khalil <65375081+1FaresKhalil@users.noreply.github.com>
2022-09-16 20:09:12 +0200

    card change

--- Frontend/src/Components/UI/Card.js
+++ Frontend/src/Components/UI/Card.js
@@ -7,7 +7,9 @@ const Card = (props) => {
         <img src={props.photo} class="w-full" alt={props.title} />
       </div>
       <div class="p-4">
-        <h5 class="text-lg md:text-2xl mb-2 uppercase">{props.title}</h5>
+        <h5 class="text-lg md:text-2xl font-bold mb-2 uppercase">
+          {props.title}
+        </h5>
         <p className="text-base py-3">{props.content}</p>
       </div>
       <button class="outlined-btn shadow shadow-red-500">

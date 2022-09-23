ded19df696c2c277d15c280a805582b9e5749ea0
1FaresKhalil <fareskhalil555@outlook.com>
2022-09-24 00:24:04 +0200

    home temp router

--- Frontend/src/Components/UI/Navbar.js
+++ Frontend/src/Components/UI/Navbar.js
@@ -6,17 +6,12 @@ import logo from "../../Assets/images/Logo.png";
 import profile from "../../Assets/images/profile.png";
 
 const Navbar = (props) => {
-  const [showMenu, setShowMenu] = useState(false);
   const [clicked, setClicked] = useState(false);
   const animateAos = !clicked ? props["data-aos"] : "";
   return (
     <nav
       data-aos={animateAos}
-      className={`lg:h-auto lg:container px-5 flex mx-auto pt-1 pb-6 rounded-xl lg:py-0 items-center justify-between w-full text-lg text-gray-700 ${
-        showMenu
-          ? "bg-white h-full content-between fixed lg:static z-10 lg:z-0 transition-opacity"
-          : "bg-transparent"
-      } lg:bg-transparent`}
+      className={`lg:h-auto lg:container px-5 flex mx-auto pt-1 pb-6 rounded-xl lg:py-0 items-center justify-between w-full text-lg text-gray-700 `}
     >
       <div>
         <Link to={"/home"}>
@@ -39,9 +34,11 @@ const Navbar = (props) => {
 
       <div className="flex justify-between items-center gap-5 rounded-[80px] bg-[#F3E1E1] pr-1 lg:pr-4">
         <div className="flex justify-between items-center gap-2  ">
-          <button className="rounded-full bg-[#F24E1E] p-2 text-center">
-            <IoLogOut className="text-white text-lg" />
-          </button>
+          <Link to={"/"}>
+            <button className="rounded-full bg-[#F24E1E] p-2 text-center">
+              <IoLogOut className="text-white text-lg" />
+            </button>
+          </Link>
           <button className="rounded-full bg-[#F24E1E] p-2 text-center">
             <IoChatbox className="text-white text-lg" />
           </button>

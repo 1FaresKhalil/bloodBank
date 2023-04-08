eabcc9ec5b514f4cc6dfe1456457684b7c61cabd
Fares Khalil <65375081+1FaresKhalil@users.noreply.github.com>
2023-04-08 23:10:44 +0200

    f

--- Frontend/src/Components/Pages/NeedBlood.js
+++ Frontend/src/Components/Pages/NeedBlood.js
@@ -3,11 +3,40 @@ import Navbar from "../UI/Navbar";
 import Logo from "../../Assets/images/Logo.png";
 import Footer from "../UI/Footer";
 function NeedBlood() {
+  const bloodRequestSubmit = async (data) => {
+    const res = await fetch("http://localhost:3002/blood-request", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res;
+  };
+  const bloodHandler = (e) => {
+    let bloodNeed = e.target.value;
+    return bloodNeed;
+  };
+  const cityHandler = (e) => {
+    let city = e.target.value;
+    return city;
+  };
+  const districtHandler = (e) => {
+    let district = e.target.value;
+    return district;
+  };
+  bloodRequestSubmit({
+    blood: bloodHandler,
+    city: cityHandler,
+    district: districtHandler,
+  }).then((res) => {
+    console.log(res);
+  });
   return (
     <div id="Need-blood">
       <Navbar />
       <div className="container mx-auto min-h-[749px] shadow-md rounded-[10px] flex justify-center pt-[117px]">
-        <form action="">
+        <form action="" onSubmit={bloodRequestSubmit}>
           <div className="text-center">
             <img className="m-auto" src={Logo} alt="logo" />
           </div>
@@ -16,7 +45,12 @@ function NeedBlood() {
               فصيله الدم اللي تحتاج اليها
             </label>
             <div className="p-[8px] border-[#D9D9D9] border-solid border-[1px] rounded-xl py-2 mt-1 w-[318px] max-w-full">
-              <select className="opacity-[40%]" name="blood" id="blood">
+              <select
+                onChange={bloodHandler}
+                className="opacity-[40%]"
+                name="blood"
+                id="blood"
+              >
                 <option value="A">A</option>
                 <option value="B">B</option>
                 <option value="AB">AB</option>
@@ -29,7 +63,12 @@ function NeedBlood() {
               المحافظة
             </label>
             <div className="p-[8px] border-[#D9D9D9] border-solid border-[1px] rounded-xl py-2 mt-1">
-              <select className="opacity-[40%]" name="city" id="city">
+              <select
+                className="opacity-[40%]"
+                name="city"
+                id="city"
+                onChange={cityHandler}
+              >
                 <option value="Alexandria">الاسكندرية</option>
                 <option value="cairo">القاهرة</option>
                 <option value="Alexandria">الاسكندرية</option>
@@ -62,6 +101,7 @@ function NeedBlood() {
 "
                 id="Neighborhood
                   "
+                onChange={districtHandler}
               >
                 <option value="Almontazah">المنترة</option>
                 <option value="Agami">العجمى</option>

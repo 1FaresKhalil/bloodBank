874388440280b17ea6e0f5cbd1918c2e9ec003d0
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-04-15 03:58:51 +0200

    backthree

--- backend/test.js
+++
@@ -1,5 +0,0 @@
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const code = crypto.randomBytes(1).toString('hex');
-token = jwt.sign({code}, 'authzzzz', {expiresIn: '60m'});
-console.log(token)

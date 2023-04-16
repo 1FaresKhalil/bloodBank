ab04d575f9abda646b973af9398db742db7b914a
Fares Khalil <65375081+1FaresKhalil@users.noreply.github.com>
2023-04-16 05:14:04 +0200

    v

--- frontend/src/pages/home/logout.tsx
+++ frontend/src/pages/home/logout.tsx
@@ -3,27 +3,16 @@ import { useRouter } from 'next/router';
 const Logout = () => {
   const router = useRouter();
 
-  // // Clear token from localStorage
-  localStorage.removeItem('token');
+  // Check if localStorage is defined before using it
+  if (typeof window !== 'undefined' && localStorage) {
+    localStorage.removeItem('token');
+  }
 
-  // // Redirect to home page after clearing token
   setTimeout(() => {
     router.replace('/');
   }, 1000);
 
-  // // Invalidate SWR cache
-  // const { mutate } = useSWR('https://gp-backend-topaz.vercel.app//admin/profile', (url) =>
-  //   fetch(url).then((res) => res.json())
-  // );
-
-  // // Call mutate function to refresh data and trigger re-render of components that use this data
-  // mutate();
-
-  return (
-    <div>
-      <h1>You are Signed out now</h1>
-    </div>
-  );
+  // Rest of your code
 };
 
 export default Logout;

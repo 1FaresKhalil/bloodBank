fe63f3634c47e7c096d0ebeda35f606de8ec3293
Ahmed Mohammed <ahmed.mohammed3588@gmail.com>
2023-05-22 13:50:14 +0300

    Update Profile

--- frontend/src/pages/home/profile.tsx
+++ frontend/src/pages/home/profile.tsx
@@ -1,4 +1,17 @@
-import { Avatar, Card, CardContent, Grid } from '@mui/material';
+import type { SelectChangeEvent } from '@mui/material';
+import {
+  Avatar,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  FormHelperText,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -17,8 +30,19 @@ import Navbar from '@/components/app-bar';
 import ErrorPage from '@/components/error';
 
 function Profile() {
+  const [editMode, setEditMode] = React.useState(false);
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [bloodType, setBloodType] = React.useState('');
+  const [phone, setPhone] = React.useState('');
+  const [nameError, setNameError] = React.useState(false);
+  const [nameHelperText, setNameHelperText] = React.useState('');
+
+  const [bloodError, setBloodError] = React.useState(false);
+  const [bloodHelperText, setBloodHelperText] = React.useState('');
+
   // Fetch profile data using SWR
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     `${process.env.NEXT_PUBLIC_DB_URI}/website/profile`,
     async (url) => {
       const token = localStorage.getItem('token');
@@ -30,22 +54,249 @@ function Profile() {
           Authorization: `${token}`,
         },
       });
-      // console.log(response.data.user);
       return response.data;
     }
   );
+
+  React.useEffect(() => {
+    if (data) {
+      // Update the state variables with the fetched data
+      setName(data?.user?.name || '');
+      setEmail(data?.user?.username || '');
+      setBloodType(data?.user?.bloodType || '');
+      setPhone(data?.user?.phone || '');
+    }
+  }, [data]);
 
   if (error) {
     return <ErrorPage />;
     // console.error(error);
   }
 
+  const handleValidation = (field: string, value: string) => {
+    if (field === 'name') {
+      if (value.trim() === '') {
+        setNameError(true);
+        setNameHelperText('Name is required');
+      } else {
+        setNameError(false);
+        setNameHelperText('');
+      }
+    }
+  };
+
+  const editUserHandler = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    handleValidation('name', name);
+    handleValidation('bloodType', bloodType);
+
+    if (nameError || bloodError) {
+      return;
+    }
+
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        throw new Error('Token not found');
+      }
+
+      const response = await axios.put(
+        `${process.env.NEXT_PUBLIC_DB_URI}/website/user/profile/update`,
+        {
+          name,
+          username: email,
+          bloodType,
+          phone,
+        },
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+
+      // Handle the response as needed
+      console.log(response.data);
+
+      // Disable edit mode after successful update
+      setEditMode(false);
+      mutate();
+    } catch (err) {
+      // Handle the error
+    }
+  };
+  const handleChange = (event: SelectChangeEvent) => {
+    setBloodType(event.target.value as string);
+    if (event.target.value === '') {
+      setBloodError(true);
+      setBloodHelperText('Please select your blood type');
+    } else {
+      setBloodError(false);
+      setBloodHelperText('');
+    }
+  };
+
+  const cancelHandler = () => {
+    setName(data?.user?.name);
+    setEmail(data?.user?.username);
+    setBloodType(data?.user?.bloodType);
+    setPhone(data?.user?.phone);
+    setEditMode(false);
+  };
+
+  if (editMode && data) {
+    return (
+      <div>
+        <Navbar username={data?.user?.username} />
+        <ThemeProvider theme={createTheme()}>
+          <div className="h-screen flex flex-col justify-between">
+            <Container component="main" maxWidth="md">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  src={'/assets/images/logo.png'}
+                  alt="logo"
+                />
+                <Typography component="h1" variant="h5">
+                  Profile
+                </Typography>
+                {data && (
+                  <Grid container spacing={2} sx={{ mt: 2 }}>
+                    <Grid item xs={12}>
+                      <Card>
+                        <CardContent>
+                          <Grid container spacing={2}>
+                            <Grid
+                              item
+                              xs={12}
+                              md={4}
+                              className="flex justify-center items-center"
+                            >
+                              <Avatar
+                                sx={{ width: 100, height: 100 }}
+                                src={data?.user?.photoURL || ''}
+                                alt="Profile Picture"
+                              />
+                            </Grid>
+                            <Grid item xs={12} md={8}>
+                              <form onSubmit={editUserHandler}>
+                                <Grid
+                                  className="flex items-center justify-between gap-2"
+                                  sx={{ marginBottom: '15px' }}
+                                >
+                                  <Button
+                                    fullWidth
+                                    variant="contained"
+                                    type="submit"
+                                  >
+                                    Save
+                                  </Button>
+                                  <Button
+                                    fullWidth
+                                    variant="contained"
+                                    color="error"
+                                    onClick={cancelHandler}
+                                  >
+                                    Cancel
+                                  </Button>
+                                </Grid>
+                                <TextField
+                                  fullWidth
+                                  error={nameError}
+                                  helperText={nameHelperText}
+                                  onChange={(e) => {
+                                    handleValidation('name', e.target.value);
+                                    setName(e.target.value);
+                                  }}
+                                  id="filled-basic"
+                                  label="Name"
+                                  value={name}
+                                  variant="filled"
+                                />
+                                <TextField
+                                  fullWidth
+                                  id="filled-basic"
+                                  label="Email"
+                                  value={email}
+                                  onChange={(e) => setEmail(e.target.value)}
+                                  variant="filled"
+                                  disabled
+                                />
+                                <FormControl fullWidth variant="filled">
+                                  <InputLabel
+                                    error={bloodError}
+                                    id="demo-simple-select-label"
+                                  >
+                                    Blood Type
+                                  </InputLabel>
+                                  <Select
+                                    error={bloodError}
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={bloodType}
+                                    label="Blood Type"
+                                    variant="filled"
+                                    onChange={handleChange}
+                                  >
+                                    <MenuItem value={'o-'}>O-</MenuItem>
+                                    <MenuItem value={'o+'}>O+</MenuItem>
+                                    <MenuItem value={'a+'}>A+</MenuItem>
+                                    <MenuItem value={'a-'}>A-</MenuItem>
+                                    <MenuItem value={'b-'}>B-</MenuItem>
+                                    <MenuItem value={'b+'}>B+</MenuItem>
+                                    <MenuItem value={'ab+'}>AB+</MenuItem>
+                                    <MenuItem value={'ab-'}>AB-</MenuItem>
+                                  </Select>
+                                  <FormHelperText error={bloodError}>
+                                    {bloodError ? bloodHelperText : ''}
+                                  </FormHelperText>
+                                </FormControl>
+                                <TextField
+                                  fullWidth
+                                  id="filled-basic"
+                                  label="Phone Number"
+                                  value={phone}
+                                  onChange={(e) => setPhone(e.target.value)}
+                                  variant="filled"
+                                />
+                              </form>
+
+                              {/* Render other profile data */}
+                            </Grid>
+                          </Grid>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                )}
+              </Box>
+            </Container>
+            <Box sx={{ mt: 8, mb: 4 }} component="footer">
+              {/* Render footer */}
+            </Box>
+          </div>
+        </ThemeProvider>
+      </div>
+    );
+  }
   return (
     <div>
       <Navbar username={data?.user?.username} />
       <ThemeProvider theme={createTheme()}>
         <div className="h-screen flex flex-col justify-between">
-          <Container component="main" maxWidth="xs">
+          <Container component="main" maxWidth="md">
             <CssBaseline />
             <Box
               sx={{
@@ -70,7 +321,12 @@ function Profile() {
                     <Card>
                       <CardContent>
                         <Grid container spacing={2}>
-                          <Grid item xs={12} md={4}>
+                          <Grid
+                            item
+                            xs={12}
+                            md={4}
+                            className="flex justify-center items-center"
+                          >
                             <Avatar
                               sx={{ width: 100, height: 100 }}
                               src={data?.user?.photoURL || ''}
@@ -78,15 +334,56 @@ function Profile() {
                             />
                           </Grid>
                           <Grid item xs={12} md={8}>
-                            <Typography variant="body1">
-                              Name: {data?.user?.name}
-                            </Typography>
-                            <Typography variant="body1">
-                              Email: {data?.user?.username}
-                            </Typography>
-                            <Typography variant="body1">
-                              Blood-type: {data?.user?.bloodType}
-                            </Typography>
+                            <FormControl fullWidth variant="filled">
+                              <Button
+                                sx={{ marginBottom: '15px' }}
+                                variant="contained"
+                                onClick={() => setEditMode(true)}
+                              >
+                                Edit
+                              </Button>
+                              <TextField
+                                label="Name"
+                                id="filled-read-only-input"
+                                value={data?.user?.name}
+                                InputProps={{
+                                  readOnly: true,
+                                }}
+                                variant="filled"
+                              />
+                              <TextField
+                                label="Email"
+                                id="filled-read-only-input"
+                                value={data?.user?.username}
+                                InputProps={{
+                                  readOnly: true,
+                                }}
+                                variant="filled"
+                              />
+                              <TextField
+                                label="Blood Type"
+                                id="filled-read-only-input"
+                                value={data?.user?.bloodType}
+                                InputProps={{
+                                  readOnly: true,
+                                }}
+                                variant="filled"
+                              />
+                              <TextField
+                                label="Phone Number"
+                                id="filled-read-only-input"
+                                value={
+                                  data?.user?.phone
+                                    ? data?.user?.phone
+                                    : 'Empty'
+                                }
+                                InputProps={{
+                                  readOnly: true,
+                                }}
+                                variant="filled"
+                              />
+                            </FormControl>
+
                             {/* Render other profile data */}
                           </Grid>
                         </Grid>
